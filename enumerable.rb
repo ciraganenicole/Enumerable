f971968357b5da02824a8c7644794a2b9006ea7b
ciraganenicole <ciraganenicole@gmail.com>
2022-06-10 17:58:10 +0200

    Create MyEnumerable module

---
+++ enumerable.rb
@@ -0,0 +1,21 @@
+module MyEnumerable
+  def all?
+    return true unless block_given?
+
+    each { |n| return false unless yield(n) }
+    true
+  end
+
+  def any?
+    return true unless block_given?
+
+    each { |n| return true if yield(n) }
+    false
+  end
+
+  def filter
+    arr = []
+    each { |n| arr.push(n) if yield(n) }
+    arr
+  end
+end
